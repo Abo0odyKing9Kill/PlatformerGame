db2296a2134742488cf8edc06ee4625db7baff22
Mouath Ibrahim <Mouath.Ibrahim@gmail.com>
2018-03-18 12:53:06 -0500

    animiation

--- PlatformerGame/Assets/Scripts/PlayerMove.cs
+++ PlatformerGame/Assets/Scripts/PlayerMove.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
 
-	public float playerSpeed = 5.0f;
+	public float PlayerSpeed = 5.0f;
+	public float playerJumpHeight = 15f;
+	private SpriteRenderer _spriteRenderer;
 
 	private Rigidbody2D _rigidbody2D;
 //	private BoxCollider2D BoxCollider2D;
@@ -15,16 +17,71 @@ public class PlayerMove : MonoBehaviour
 		//print("Abdullah is Making Video Game\nCalled in Start() Function");
 		_rigidbody2D = GetComponent<Rigidbody2D>();
 //		BoxCollider2D = GetComponent<BoxCollider2D>();
+		_spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//print("Abdullah is Playing Video Game\n\tCalled in Uodate() Function");
-		float moveLEFT_RIGHT = Input.GetAxis("Horizontal"); // A OR D -> or <-
-		float moveUP_DOWN = Input.GetAxis("Vertical"); // W OR S ^ or v
+//		//print("Abdullah is Playing Video Game\n\tCalled in Uodate() Function");
+		float moveLeftRight = Input.GetAxis("Horizontal"); // A OR D -> or <-
+		float moveUpDown = Input.GetAxis("Vertical"); // W OR S ^ or v
 		
-		Vector2 movements = new Vector2(moveLEFT_RIGHT, moveUP_DOWN);
-		_rigidbody2D.AddForce(movements * playerSpeed);
+		Vector2 movements = new Vector2(moveLeftRight, 0);
+		_rigidbody2D.AddForce(movements * PlayerSpeed);
+		print(Input.GetAxis("Horizontal"));
 		
+		if (Input.GetAxis("Horizontal") < 0)
+		{
+			
+			_spriteRenderer.flipX = true;
+		}
+		else
+		{
+			_spriteRenderer.flipX = false;
+		}
+
+		movePlayer();
+		
+	}
+
+	void movePlayer()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			//Make the player JUMP
+			print("SPACE Pressed");
+			//new Vector2(0, 10), ForceMode2D.Impulse);
+			_rigidbody2D.AddForce(new Vector2(0, playerJumpHeight), ForceMode2D.Impulse);
+		}
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+			//Make the player crotch or climb down
+			print("S Pressed");
+		}
+
+//		if (Input.GetKeyDown(KeyCode.D))
+//		{
+//			float moveLeftRight = Input.GetAxis("Horizontal");
+//			//Make the player move Right
+//			print("D Pressed");
+//			
+//			Vector2 movements = new Vector2(moveLeftRight, 0);
+////			_rigidbody2D.AddForce(movements * PlayerSpeed);
+////			_rigidbody2D.AddForce(new Vector2(moveLeftRight, 0) * PlayerSpeed);
+//		}
+//
+//		if (Input.GetKeyDown(KeyCode.A))
+//		{
+//			float moveLeftRight = Input.GetAxis("Horizontal");
+//			//Make the player move LEFT
+//			print("A Pressed");
+//			_rigidbody2D.AddForce(new Vector2(moveLeftRight, 0) * PlayerSpeed);
+//		}
+
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			//Make the player climb up 
+			print("W Pressed");
+		}
 	}
 }
