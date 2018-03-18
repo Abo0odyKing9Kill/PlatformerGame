9961f65eba22f20d1a48f899c2257c1a4c426b67
Mouath Ibrahim <Mouath.Ibrahim@gmail.com>
2018-03-18 13:53:44 -0500

    Better Player movments and respwaning on fall
    Using https://github.com/prime31/CharacterController2D

---
+++ PlatformerGame/Assets/Scripts/PlayerScripts/PlayerFall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFall : MonoBehaviour
+{
+
+	public float threshold;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+		if (transform.position.y < threshold)
+		{
+			//player fell to death ... respawn
+			transform.position = new Vector2(0,0);
+			print("You fell to your DEATH!");
+		}
+	}
+}
